7733c13571ccbbe0cd3dc73e424de6546d968f6c
nikolicmila <38696241+nikolicmila@users.noreply.github.com>
2018-09-12 11:42:06 +0200

    Add files via upload

---
+++ LEDTimer.ino
@@ -0,0 +1,51 @@
+#include <TimerOne.h>
+
+int procent;
+long int period;
+const byte Led=13;
+
+void setup(){
+  pinMode(Led, OUTPUT);
+  Serial.begin(9600);
+  
+
+  Timer1.attachInterrupt( timerIsr );
+  period = 500 * 1000L;
+  procent = 50;
+}
+
+void loop()
+{
+  Serial.print("period: ");
+  Serial.print(period);
+  Serial.print(" procent: ");
+  Serial.println(procent);
+  
+  if(Serial.available()){
+    switch(Serial.read()) {
+      
+      case 'c' : 
+      period=Serial.parseInt();
+      break;
+      
+      case 'p' :
+      procent=Serial.parseInt();
+      break;
+    }
+  }
+  delay(500);
+}
+
+void timerIsr(){
+  
+  if(digitalRead(13) == LOW){
+    Timer1.initialize(period/100*procent);
+    digitalWrite( 13, HIGH);
+  }
+  
+  else {
+     Timer1.initialize(period-(period/100*procent));
+     digitalWrite(13,LOW);
+  }
+
+}
---
+++ pupravuvac.ino
@@ -0,0 +1,29 @@
+int kp;
+float setTemp;
+int val;
+int tempPin = A0;
+
+void setup()
+{
+setTemp=26;
+Serial.begin(9600);
+}
+void loop()
+{
+val = analogRead(tempPin);
+float mv = ( val/1024.0)*5000; 
+float cel = mv/10;
+
+double error = setTemp - cel;
+double output = kp * error; 
+
+Serial.print("TEMPRATURE = ");
+Serial.print(cel);
+Serial.print("*C");
+Serial.println();
+
+Serial.print("output = ");
+Serial.print(output);
+Serial.println();
+delay(1000);
+}
