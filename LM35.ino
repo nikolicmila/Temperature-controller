0f6e4cd630474720102eab07ad8a03ebb57c21f6
Mila Nikolikj <38696241+nikolicmila@users.noreply.github.com>
2018-09-14 15:59:47 +0200

    Update LM35.ino

--- LM35.ino
+++ LM35.ino
@@ -8,19 +8,11 @@ Serial.begin(9600);
 void loop()
 {
 val = analogRead(tempPin);
-float mv = ( val/1024.0)*5000; 
-float cel = mv/10;
-/* float farh = (cel*9)/5 + 32; */
+float cel = val*0.48828125;
 
 Serial.print("TEMPRATURE = ");
 Serial.print(cel);
 Serial.print("*C");
 Serial.println();
 delay(1000);}
-
-/* uncomment this to get temperature in farenhite 
-Serial.print("TEMPRATURE = ");
-Serial.print(farh);
-Serial.print("*F");
-Serial.println(); */
 
